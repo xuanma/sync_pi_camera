cd876290cc9f8419e5253c868eb64c1037b29979
xuanma <maxuan.1987@163.com>
2019-03-02 22:49:30 -0600

    new recording script

--- camera_preview_only.py
+++ camera_preview_only.py
@@ -2,7 +2,8 @@ import picamera
 import time
  
 with picamera.PiCamera() as camera:
-    camera.resolution = (1024,768)	# resolution
+    camera.resolution = (1280, 720)	# resolution
+    camera.rotation =90
     camera.framerate = 30			# frame rate
     print ("start preview direct from GPU")
     camera.start_preview() 	# the start_preview() function 
--- sync_pts.py
+++ sync_pts.py
@@ -1,11 +1,33 @@
 import RPi.GPIO as GPIO
-import time,sys,os
+import time,sys,os, io, time
 from picamera import PiCamera
 from signal import pause
-from time import sleep
+
+class PtsOutput(object):
+	def __init__(self, camera, video_filename, pts_filename):
+		self.camera = camera
+		self.video_output = io.open(video_filename, 'wb')
+		self.pts_output = io.open(pts_filename, 'w')
+		self.start_time = None
+		
+	def write(self, buf):
+		self.video_output.write(buf)
+		if self.camera.frame.complete and self.camera.frame.timestamp:
+			if self.start_time is None:
+				self.start_time = time.time()
+			self.pts_output.write(u'%f\n' % (time.time() - self.start_time))
+			
+	def flush(self):
+		self.video_output.flush()
+		self.pts_output.flush()
+		
+	def close(self):
+		self.video_output.close()
+		self.pts_output.close()	
 
 NUM = '_05'
 task_name = 'cage_'
+recording_time = 900
 camera = PiCamera()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -58,17 +80,6 @@ while True:
 		else:
 			detected_start = 0
 			N = 0
-
-    if GPIO.event_detected(20):
-		if GPIO.input(21) == GPIO.HIGH:
-			if GPIO.input(20) == GPIO.HIGH:
-				N = N+1
-				if N == 3:
-					detected_stop = 1
-					N = 0
-		else:
-			detected_stop = 0
-			N = 0
 			
     if state == 0:
 		if detected_start == 1:
@@ -81,17 +92,12 @@ while True:
 			
 			t_str = ''.join(t_str)
 			video_savename = ''.join((path_for_video, video_base_name, t_str, NUM, '.h264'))
-			camera.start_preview()
-			camera.start_recording(video_savename)
-			
-			detected_start = 0
-    elif state == 1:
-		if detected_stop == 1:
-			state = 0
+			pts_savename = ''.join((path_for_video, video_base_name, t_str, NUM, '.txt'))
+			print('Recording...')
+			camera.start_recording(PtsOutput(camera, video_savename, pts_savename), format='h264')
+			camera.wait_recording(recording_time)
 			camera.stop_recording()
-			camera.stop_preview()
-			detected_stop = 0
-		print('')
-			 
-	   
+			print('Done') 
+			detected_start = 0
+			state = 0  
 GPIO.cleanup()
